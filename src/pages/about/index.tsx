a96f267254d41e477b59091735a70ec1c0e6ff58
JD Ghuman <jdshguman@gmail.com>
2022-01-29 19:47:19 -0600

    Update file structure

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,3 +1,56 @@
-import About from './About'
+import Link from 'next/link'
+import styles from './About.module.scss'
+import { AiFillGithub, AiOutlineMail, AiFillLinkedin } from 'react-icons/ai'
+import Meta from '@components/Meta'
+import { MetaTags, PageType, RobotsContent } from '@components/PropTypes/Tags'
+
+const About = () => {
+  const postMetaTags: MetaTags = {
+    canonical: 'https://www.jdghuman.com',
+    description: `Software Engineer | Front End Engingeer based out of Houston, TX`,
+    image: 'https://www.jdghuman.com/images/JD-Develop.png',
+    robots: `${RobotsContent.follow},${RobotsContent.index}`,
+    title: `jdGhuman | About`,
+    type: PageType.website,
+  }
+
+  return (
+    <>
+      <Meta tags={postMetaTags} />
+      <div className={styles.section}>
+        <div className={styles.section__container}>
+          <h2 className={styles.name}>About me</h2>
+          <p className={styles.detail}>
+            I am a Software Engineer who loves to code and build things, located in Houston, TX. I
+            enjoy turning concepts and designs into front end UI and solving complex problems with
+            code.
+          </p>
+          <p>
+            When I am not building web applications or coding, I can be found hanging out with my
+            family, building something in the garage, or drinking coffee…. I’m always drinking
+            coffee!
+          </p>
+          <div className={styles.social}>
+            <Link href="https://github.com/jdsghuman">
+              <a target="_blank">
+                <AiFillGithub className={styles.social__icon} />
+              </a>
+            </Link>
+            <Link href="mailto:jdsghuman@gmail.com">
+              <a target="_blank">
+                <AiOutlineMail className={styles.social__icon} />
+              </a>
+            </Link>
+            <Link href="https://www.linkedin.com/in/jdghuman">
+              <a target="_blank">
+                <AiFillLinkedin className={styles.social__icon} />
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default About
